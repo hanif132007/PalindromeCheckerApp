6b36b341bc82913f7756156ec729135e992e9cef
hanif132007 <hanif132007@gmail.com>
2026-02-25 16:07:58 +0530

    welcome to palindrome app

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerApp{
+    public static void main() {
+        System.out.println("===========");
+        System.out.println("Welcome to palindrome checker app");
+        System.out.println("version 1.0...");
+        System.out.println("=========");
+    }
+}
